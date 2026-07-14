0fcd114e523e6d801f8ed9c487603cb980f3742b
Parks RPK <prpk@binghamton.edu>
2026-07-14 08:45:46 -0400

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        intervals.sort(key = lambda x : x.start, reverse = False)
+        heap = []
+        heapq.heappush(heap, intervals[0].end )
+        for i in range(1,len(intervals)):
+            if intervals[i].start >= heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, intervals[i].end)
+        return len(heap)
